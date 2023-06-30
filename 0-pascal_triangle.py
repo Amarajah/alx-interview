0afa6d90251e1b0488c329efeb0dd1235acd886f
Amarajah <naomi.ibeh69@gmail.com>
2023-06-30 16:04:11 +0000

    Solution to Pascal's triangle interview question

--- 0-pascal_triangle.py
+++ 0-pascal_triangle.py
@@ -2,5 +2,17 @@ def pascal_triangle(n):
     """returns a list of lists of integers."""
     if (n <= 0):
         return []
-    else:
 
+    triangle = [[1]]
+
+    for i in range(1, n):
+        prev_row = triangle[i - 1]
+        curr_row = [1]
+
+        for j in range(1, i):
+            curr_row.append(prev_row[j - 1] + prev_row[j])
+
+        curr_row.append(1)
+        triangle.append(curr_row)
+
+        return triangle
