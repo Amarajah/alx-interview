ff5b00344f27826638f73417f353640c372551ba
Amarajah <naomi.ibeh69@gmail.com>
2023-07-06 23:00:52 +0000

    locked boxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -4,7 +4,7 @@
 
 def canUnlockAll(boxes):
     """Method that determines if all the boxes can be opened."""
-    unlocked = [boxes[0]
+    unlocked = [boxes[0]]
     for box in unlocked:
         for b in box:
             if b < len(boxes) and boxes[b] not in unlocked:
