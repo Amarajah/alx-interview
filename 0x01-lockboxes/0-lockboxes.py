704586dd8ca53069e0fab0785f0e76e12bcb28e4
Amarajah <naomi.ibeh69@gmail.com>
2023-07-06 22:58:19 +0000

    locked boxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,12 @@
+#/usr/bin/python3
+"""0-lockboxes."""
+
+
+def canUnlockAll(boxes):
+    """Method that determines if all the boxes can be opened."""
+    unlocked = [boxes[0]
+    for box in unlocked:
+        for b in box:
+            if b < len(boxes) and boxes[b] not in unlocked:
+                unlocked.append(boxes[b])
+    return len(unlocked) == len(boxes)
